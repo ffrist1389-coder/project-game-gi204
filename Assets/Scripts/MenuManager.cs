5bdbff5d28d6b8391f4d687d8b306bd5330d9695
ffrist1389-coder <ffrist1389@gmail.com>
2026-03-16 23:39:31 +0700

    เสร็จแล้ว

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,8 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject howToPlayPanel;
+
     public void StartGame()
     {
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void ShowHowToPlay()
+    {
+        howToPlayPanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
